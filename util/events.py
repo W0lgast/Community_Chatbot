31ca261a37528410f9d4e65e7532450da7b1a433
Stefan Radic Webster <hn19405@IT042479.wks.bris.ac.uk>
2020-02-06 11:06:09 +0000

    Sub genres added

--- util/events.py
+++ util/events.py
@@ -13,6 +13,19 @@ import util.utilities as ut
 
 # --------------------------------
 
+GENRE_SYNONYMS = {
+    "Music": ["folk", "indie", "jazz", "acoustic", "rock", "country", "world", "pop", "rapandhiphop", "ska", "metal",
+              "1960s", "1970s", "1980", "1990", "americana", "punk", "alternative", "rocknroll", "covers", "tribute",
+              "blues", "rnbandsoul", "electronic"],
+    "Standup Comedy": ["comedy"],
+    "Theatre": ["theatreandarts", "play", "opera", "musical", "panto", "ballet", "ballroom", "westend"],
+    "Shows & Events": ["adult", "cabaret", "showsandevents", "talk", "family", "circus", "magic", "poetry",
+                       "sports", "tv", "online", "kids"],
+    "Festivals": [],
+    "Cinema": []}
+
+# --------------------------------
+
 CLIENT_ID = "2e16b35a18c40cf04711903c1efddfa03e315cd4"
 CLIENT_SECRET = "4a7ba6ef8ea6c792e41f716087e6796ce530de08"
 AUTHORIZATION_URL = 'https://api.ents24.com/auth/token'
@@ -25,7 +38,7 @@ AUTH_DICT = ut.parseStr(response._content.decode())
 # public functions
 # -----------------------------------------------------------------------------------------
 
-def getEventsList(location="postcode:BS4 1WH",
+def getEventsList(location="postcode:BS4 1NL",
                   radius_distance=5,
                   date=None,
                   genre=None):
